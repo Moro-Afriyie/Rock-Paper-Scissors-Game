567817a8771172aac8fd7e2ee72895fd7460eb77
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-02 22:46:11 +0000

    done creating all the icons for the game

--- rock-paper-scissors-game/src/App.tsx
+++ rock-paper-scissors-game/src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
 import './App.css';
+import Lizard from './components/icons/Lizard';
+import Paper from './components/icons/Paper';
 import Rock from './components/icons/Rock';
+import Scissors from './components/icons/Scissors';
+import Spock from './components/icons/Spock';
 
 function App() {
   return (
     <div className="App">
      <Rock classType="circle" />
+     <Paper classType="circle" />
+     <Scissors classType="circle" />
+     <Lizard classType="circle" />
+     <Spock classType="circle" />
     </div>
   );
 }
