db47318f1b57cce7c50ab4b82e06a9388c82de27
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-02 22:03:45 +0000

    created a Rock component to display to rock icon

---
+++ rock-paper-scissors-game/src/components/Rock.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+const Rock: React.FC = () => {
+  return ( <div className="circle">
+      <div className="inner-circle">
+        <img src="../images/icon-rock.svg" alt=""/>
+      </div>
+    </div>);
+};
+
+export default  Rock;
