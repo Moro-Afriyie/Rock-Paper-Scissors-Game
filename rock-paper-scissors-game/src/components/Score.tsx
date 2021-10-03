1cb326f3f4ea36c97c3e1b2b4dd2b1eb1755568a
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-03 17:34:51 +0000

    imported the logo from the images folder

--- rock-paper-scissors-game/src/components/Score.tsx
+++ rock-paper-scissors-game/src/components/Score.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { ScoreProps } from '../models/interface';
+import logo from "../images/logo-bonus.svg";
 
 const Score: React.FunctionComponent< ScoreProps> = (props) => {
   return(<h1>Score</h1>) ;
