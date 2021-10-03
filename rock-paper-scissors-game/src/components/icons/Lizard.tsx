625f26fbc5960fa1a07f4025d5bb82211e5404ac
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-03 11:47:04 +0000

    modified the handleToggleDisplay function

--- rock-paper-scissors-game/src/components/icons/Lizard.tsx
+++ rock-paper-scissors-game/src/components/icons/Lizard.tsx
@@ -6,7 +6,7 @@ const Lizard: React.FunctionComponent< ClassTypeProp> = (props) => {
   const {classType, handleToggleDisplay}= props;
   const handleToggle =()=>{
     if(handleToggleDisplay!== undefined){
-      handleToggleDisplay(false,"lizard");
+      handleToggleDisplay(true,"lizard");
     }
   }
   return ( <div 
