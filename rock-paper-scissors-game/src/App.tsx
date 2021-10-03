5326068500e62700583e5df509499b5523a87387
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-03 11:44:49 +0000

    modified the handleToggleDisplay function

--- rock-paper-scissors-game/src/App.tsx
+++ rock-paper-scissors-game/src/App.tsx
@@ -1,26 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import GamePage from './components/GamePage';
 import Pentagon from './components/Pentagon';
 import Score from './components/Score';
-// import Lizard from './components/icons/Lizard';
-// import Paper from './components/icons/Paper';
-// import Rock from './components/icons/Rock';
-// import Scissors from './components/icons/Scissors';
-// import Spock from './components/icons/Spock';
+
 
 function App() {
   const [toggleDisplay, setToggleDisplay] = useState(false);
+  
   const [iconSelected, setIconSelected] = useState('');
 
    const handleToggleDisplay= (value:boolean, icon:string):void =>{
     setToggleDisplay(value);
     setIconSelected(icon);
   }
-
-  // useEffect(()=>{
-  //   handleToggleDisplay(toggleDisplay,iconSelected);
-  // },[iconSelected, toggleDisplay]);
  
   return (
     <main className="container">
