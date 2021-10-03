a7da77f38f4cc6335b592b03b7871170479e392f
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-03 18:47:58 +0000

    styled the Score component

--- rock-paper-scissors-game/src/components/Score.tsx
+++ rock-paper-scissors-game/src/components/Score.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import { ScoreProps } from '../models/interface';
-import logo from "../images/logo-bonus.svg";
+import logo from '../images/logo-bonus.svg';
 
 const Score: React.FunctionComponent< ScoreProps> = (props) => {
-  return(<h1>Score</h1>) ;
+  return(
+  <section className="score-container">
+      <div className="score-names">
+          <img src={logo} alt=""  />
+      </div>
+      <div className="score">
+        <p>Score</p>
+        <h1>12</h1>
+      </div>
+  </section>) ;
 };
 
 export default Score;
