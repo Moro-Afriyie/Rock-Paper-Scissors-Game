79339902f6b4e0b9972073538d932114f7f3c936
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-04 00:32:14 +0000

    styled the pentagon component

--- rock-paper-scissors-game/src/components/Pentagon.tsx
+++ rock-paper-scissors-game/src/components/Pentagon.tsx
@@ -5,14 +5,12 @@ import Paper from './icons/Paper';
 import Rock from './icons/Rock';
 import Scissors from './icons/Scissors';
 import Spock from './icons/Spock';
-import pentagon from '../images/bg-pentagon.svg';
 
 
 
 const Pentagon: React.FunctionComponent< PentagonProps> = ({handleToggleDisplay}) => {
   return (
     <section className="pentagon-container">
-        {/* <img src={pentagon} alt="pentagon" /> */}
         <Rock classType="circle rock" handleToggleDisplay={handleToggleDisplay}/>
         <Paper classType="circle paper" handleToggleDisplay={handleToggleDisplay}/>
         <Scissors classType="circle scissors" handleToggleDisplay={handleToggleDisplay}/>
