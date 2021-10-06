686b8aa1e2f8041c0710cb3fa97b7222d6cbc1c6
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-06 10:36:59 +0000

    implemented the switching logic

--- rock-paper-scissors-game/src/components/Pentagon.tsx
+++ rock-paper-scissors-game/src/components/Pentagon.tsx
@@ -8,14 +8,14 @@ import Spock from './icons/Spock';
 
 
 
-const Pentagon: React.FunctionComponent< PentagonProps> = ({handleToggleDisplay}) => {
+const Pentagon: React.FunctionComponent< PentagonProps> = ({toggle, handleToggleDisplay}) => {
   return (
-    <section className="pentagon-container">
+    <section className={toggle?"pentagon-container":"triangle-container"}>
         <Rock classType="circle rock" handleToggleDisplay={handleToggleDisplay}/>
         <Paper classType="circle paper" handleToggleDisplay={handleToggleDisplay}/>
         <Scissors classType="circle scissors" handleToggleDisplay={handleToggleDisplay}/>
-        <Lizard classType="circle lizard" handleToggleDisplay={handleToggleDisplay}/>
-        <Spock classType="circle spock" handleToggleDisplay={handleToggleDisplay} />
+        {toggle && <Lizard classType="circle lizard" handleToggleDisplay={handleToggleDisplay}/>}
+        {toggle && <Spock classType="circle spock" handleToggleDisplay={handleToggleDisplay} />}
      </section>
      );
 };
